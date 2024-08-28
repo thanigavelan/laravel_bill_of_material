6e9cebd0f5dda66977df99c899ed45c332f69aa4
thanigavelan <thanigavelan1305@gmail.com>
2024-08-28 18:06:29 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,7 @@ Route::get('/',[HomeController::class, 'index'])->name('home.index');
 
 // Product detail page
 Route::get('/products/{id}', [HomeController::class, 'show'])->name('product.show');
+
+use App\Http\Controllers\InvoiceController;
+
+Route::get('/invoice/{productId}', [InvoiceController::class, 'generateInvoice'])->name('invoice.generate');
