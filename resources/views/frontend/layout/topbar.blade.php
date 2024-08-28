a721c0b8b432ce570768b7be02016dbc02141a55
thanigavelan <thanigavelan1305@gmail.com>
2024-08-28 12:05:41 +0530

    Update topbar.blade.php

--- resources/views/frontend/layout/topbar.blade.php
+++ resources/views/frontend/layout/topbar.blade.php
@@ -1,1 +1,7 @@
-<h1>top bar</h1>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Small Shop</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    
+</nav>
