bb36089436ffd7130ba828b0878e364a6762949f
thanigavelan <thanigavelan1305@gmail.com>
2024-08-28 12:06:01 +0530

    Update home.blade.php

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -9,4 +9,26 @@ Forget Password
 @section('content')
 
 home
+
+<div class="container mt-4">
+
+    <div class="row">
+        @foreach ($product as $item)
+            <div class="col-md-4 mb-4">
+                <a href="/products/{{ $item->id }}">
+                    <div class="card">
+                        <img src="{{ $item->GetImagePath() }}" class="card-img-top" alt="{{ $item->name }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $item->name }}</h5>
+                            <p class="card-text">{{ $item->description }}</p>
+                            <!-- <p class="card-text"><strong>${{ $item->price }}</strong></p> -->
+                            <!-- <a href="/cart" class="btn btn-primary">Add to Cart</a> -->
+                        </div>
+                    </div>
+                </a>
+            </div>
+        @endforeach
+    </div>
+</div>
+
 @endsection
