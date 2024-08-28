ac4ef1025c04d78846ef487b605daac80d3b231b
thanigavelan <thanigavelan1305@gmail.com>
2024-08-28 12:08:49 +0530

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Product;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 class HomeController extends Controller
 {
     //
-
     public function index(Request $request)
     {
-        return view('frontend/home');
+        $data = [
+            'product' => Product::all()
+        ];
+
+        return view('frontend/home', $data);
+    }
+
+    public function show($id)
+    {
+        try {
+            
+            $data = [
+                'product' => Product::findOrFail($id),
+            ];
+
+            // dd($data);
+
+            return view('frontend.show', $data);
+        } catch (ModelNotFoundException $e) {
+            // Handle the case where the product is not found
+            return redirect()->route('products.index')->with('error', 'Product not found.');
+        }
     }
 }
